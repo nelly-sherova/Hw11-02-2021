4902273de122b1cb6919b572d860f7cb2e772de6
unknown <nellysherova@mail.ru>
2021-02-11 23:47:04 +0300

    Добавила метод Sum

--- DelegateHw/Program.cs
+++ DelegateHw/Program.cs
@@ -4,6 +4,10 @@ namespace DelegateHw
 {
     class Program
     {
+        private static T Sum<T>(T num1, T num2)
+        {
+            return (dynamic)num1 + (dynamic)num2;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
