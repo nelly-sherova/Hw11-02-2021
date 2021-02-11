1f613939fcc22c348a3514c54e38b5ff87d83193
unknown <nellysherova@mail.ru>
2021-02-12 00:27:44 +0300

    Создала делегат и поработала с ним в методе Main

--- DelegateHw/Program.cs
+++ DelegateHw/Program.cs
@@ -5,6 +5,7 @@ namespace DelegateHw
 {
     class Program
     {
+        delegate T Operation<T, K, L> (K num1, L num2);
         private static T Sum<T>(T num1, T num2)
         {
             return (dynamic)num1 + (dynamic)num2;
@@ -27,7 +28,32 @@ namespace DelegateHw
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Operation<int, int, int> operations = new Operation<int, int, int>(Sum);
+            Console.Write("number_1 = "); int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("operation: "); char oper = Convert.ToChar(Console.ReadLine());
+            Console.Write("number_2 = "); int num2 = Convert.ToInt32(Console.ReadLine());
+            switch (oper)
+            {
+                case '+':
+                    operations += Sum;
+                    Console.WriteLine($"{num1} + {num2} = {operations?.Invoke(num1, num2)}");
+                break;
+                case '-':
+                    operations += Minus;
+                    Console.WriteLine($"{num1} - {num2} = {operations?.Invoke(num1, num2)}");
+                break;
+                case '*':
+                    operations += Product;
+                    Console.WriteLine($"{num1} * {num2} = {operations?.Invoke(num1, num2)}");
+                break;
+                case '/':
+                    operations += Division;
+                    Console.WriteLine($"{num1} / {num2} = {operations?.Invoke(num1, num2)}");
+                break;
+                default:
+                    Console.WriteLine("Неправильный символ!");
+                break;
+            }
         }
     }
 }
