06df9631352251704339ef8447b728754f3bd3b8
unknown <nellysherova@mail.ru>
2021-02-11 23:49:09 +0300

    Добавила метод minus

--- DelegateHw/Program.cs
+++ DelegateHw/Program.cs
@@ -8,6 +8,10 @@ namespace DelegateHw
         {
             return (dynamic)num1 + (dynamic)num2;
         }
+        private static T Minus<T>(T num1, T num2)
+        {
+            return (dynamic)num1 - (dynamic)num2;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
