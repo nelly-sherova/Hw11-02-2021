beeb02ee24a14ecd9b8b911a8b65c55b19da3a0c
unknown <nellysherova@mail.ru>
2021-02-12 10:19:11 +0300

    Финальное решение задачи

--- DelegateHw/Program.cs
+++ DelegateHw/Program.cs
@@ -5,7 +5,7 @@ namespace DelegateHw
 {
     class Program
     {
-        delegate T Operation<T, K, L> (K num1, L num2);
+        delegate T Operation<T> (T num1, T num2);
         private static T Sum<T>(T num1, T num2)
         {
             return (dynamic)num1 + (dynamic)num2;
@@ -25,29 +25,28 @@ namespace DelegateHw
             else
                 return (dynamic)num1 / (dynamic)num2;
         }
-
         static void Main(string[] args)
         {
-            Operation<int, int, int> operations = new Operation<int, int, int>(Sum);
-            Console.Write("number_1 = "); int num1 = Convert.ToInt32(Console.ReadLine());
+            Operation<double> operations;
+            Console.Write("number_1 = "); double.TryParse(Console.ReadLine(), out double num1);
             Console.Write("operation: "); char oper = Convert.ToChar(Console.ReadLine());
-            Console.Write("number_2 = "); int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("number_2 = "); double.TryParse(Console.ReadLine(), out double num2);
             switch (oper)
             {
                 case '+':
-                    operations += Sum;
+                    operations = Sum;
                     Console.WriteLine($"{num1} + {num2} = {operations?.Invoke(num1, num2)}");
                 break;
                 case '-':
-                    operations += Minus;
+                    operations = Minus;
                     Console.WriteLine($"{num1} - {num2} = {operations?.Invoke(num1, num2)}");
                 break;
                 case '*':
-                    operations += Product;
+                    operations = Product;
                     Console.WriteLine($"{num1} * {num2} = {operations?.Invoke(num1, num2)}");
                 break;
                 case '/':
-                    operations += Division;
+                    operations = Division;
                     Console.WriteLine($"{num1} / {num2} = {operations?.Invoke(num1, num2)}");
                 break;
                 default:
